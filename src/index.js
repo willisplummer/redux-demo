4c9cb797773bc4be08f826bb8445c075846e0839
Willis Plummer <willisplummer@gmail.com>
2017-01-16 08:07:58 -0500

    middleware experiment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 
 // UPDATE
 
@@ -28,9 +28,27 @@ const Counter = ({ value, onIncrement, onDecrement, onReset }) => (
   </div>
 );
 
+// MIDDLEWARE
+
+const createLogger = ({ getState }) => {
+  return (next) =>
+    (action) => {
+      const console = window.console;
+      const prevState = getState();
+      const returnValue = next(action);
+      const nextState = getState();
+      const actionType = String(action.type);
+      const message = `action ${actionType}`;
+      console.log(`%c prev state`, `color: #9E9E9E`, prevState);
+      console.log(`%c action`, `color: #03A9F4`, action);
+      console.log(`%c next state`, `color: #4CAF50`, nextState);
+      return returnValue;
+    };
+}
+
 // INIT
 
-const store = createStore(update, 0);
+const store = createStore(update, 0, applyMiddleware(createLogger));
 const rootEl = document.getElementById('root');
 
 const render = () => {
