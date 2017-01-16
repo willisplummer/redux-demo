cd6d86b6433cf656429f0bc84f67d5a58000116c
Willis Plummer <willisplummer@gmail.com>
2017-01-16 12:01:12 -0500

    closest ive come to getting redux n draft to play nice

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
+import { Editor, EditorState } from 'draft-js';
 
 // UPDATE
 
 const update = (state = 0, action) => {
   switch (action.type) {
+    case 'EDITOR_CHANGE':
+      console.log('editor change');
+      return { count: state.count, editorState: action.editorState };
     case 'INCREMENT':
-      return state + 1;
+      return { state, ...{ count: state.count + 1 }};
     case 'DECREMENT':
-      return state - 1;
-    case 'RESET':
-      return state = 0;
+      return { state, ...{ count: state.count - 1 }};
     default:
       return state;
   }
@@ -19,27 +21,36 @@ const update = (state = 0, action) => {
 
 // VIEW
 
-const Counter = ({ value, onIncrement, onDecrement, onReset }) => (
+const Main = ({ state, onIncrement, onDecrement, onEditorChange }) => {
+  const { count, editorState } = state
+  return(
+    <div>
+      <Counter { ...{ count, onIncrement, onDecrement } } />
+      <Editor onChange={onEditorChange} { ...{ editorState } } />
+    </div>
+  )
+}
+
+const Counter = ({ count, onIncrement, onDecrement }) => (
   <div>
-    <p>Count: {value}</p>
+    <p>Count: {count}</p>
     <button onClick={onIncrement}>+</button>
     <button onClick={onDecrement}>-</button>
-    <button onClick={onReset}>reset</button>
   </div>
 );
 
 // INIT
 
-const store = createStore(update, 0);
+const store = createStore(update, { count: 0, editorState: EditorState.createEmpty()});
 const rootEl = document.getElementById('root');
 
 const render = () => {
   ReactDOM.render(
-    <Counter
-      value={store.getState()}
+    <Main
+      state={store.getState()}
       onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
       onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-      onReset={() => store.dispatch({ type: 'RESET' })}
+      onEditorChange={(editorState) => store.dispatch({ type: 'EDITOR_CHANGE', editorState: editorState})}
     />,
     rootEl
   );
