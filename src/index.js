11d2b2de574d44b4c4c2dae683c43b65b4d457f5
willis plummer <willisplummer@gmail.com>
2017-01-19 15:05:37 -0500

    add image embeds

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import { CompositeDecorator, Editor, EditorState, Entity, RichUtils } from 'draft-js';
+import { CompositeDecorator, convertToRaw, Editor, EditorState, Entity, RichUtils } from 'draft-js';
 import { stateToHTML } from 'draft-js-export-html';
 import { stateFromHTML } from 'draft-js-import-html';
 
@@ -47,6 +47,10 @@ class MyEditor extends React.Component {
         strategy: findLinkEntities,
         component: Link,
       },
+      {
+        strategy: findImageEntities,
+        component: Image,
+      },
     ]);
 
     let editorState;
@@ -59,7 +63,9 @@ class MyEditor extends React.Component {
     this.state = {
       editorState: editorState,
       showURLInput: false,
-      urlValue: ''
+      urlValue: '',
+      imgSrc: '',
+      showImgInput: false
     };
 
     const updateStore = createDebouncer((editorState) => store.dispatch({
@@ -68,33 +74,66 @@ class MyEditor extends React.Component {
     }), 500)
 
     this.onChange = (editorState) => {
-      updateStore(editorState)
       this.setState({editorState})
+      updateStore(editorState)
     };
+
+    this.logState = () => {
+      const content = this.state.editorState.getCurrentContent();
+      console.log(convertToRaw(content));
+    };
+
     this.onURLChange = (e) => this.setState({ urlValue: e.target.value });
-
     this.confirmLink = this._confirmLink.bind(this);
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
-  }
+    this.promptForLink = this._promptForLink.bind(this)
+    this.onLinkInputKeyDown = this._onLinkInputKeyDown.bind(this);
 
+    this.onImgChange = (e) => this.setState({ imgSrc: e.target.value });
+    this.confirmImg = this._confirmImg.bind(this);
+    this.promptForImgSrc = this._promptForImgSrc.bind(this);
+    this.onImgInputKeyDown = this._onImgInputKeyDown.bind(this);
+
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    this.removeLink = this._removeLink.bind(this);
+    this.onTab = (e) => this._onTab(e);
+    this.toggleInlineStyle = (type) => this._toggleInlineStyle(type);
+    this.toggleBlockType = (type) => this._toggleBlockType(type);
+  }
 
   handleKeyCommand(command) {
-    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    const {editorState} = this.state;
+    const newState = RichUtils.handleKeyCommand(editorState, command);
     if (newState) {
       this.onChange(newState);
-      return 'handled';
+      return true;
     }
-    return 'not-handled';
+    return false;
   }
 
-  _onBoldClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+
+// not working as expected (nothing happens) :thinking_face:
+  _onTab(e) {
+    e.preventDefault();
+    const maxDepth = 4;
+    this.onChange(RichUtils.onTab(e, this.state.editorState, maxDepth));
   }
-  _onItalicClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'ITALIC'));
+
+  _toggleInlineStyle(inlineStyle) {
+    this.onChange(
+      RichUtils.toggleInlineStyle(
+        this.state.editorState,
+        inlineStyle
+      )
+    );
   }
-  _onUnderlineClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'UNDERLINE'));
+
+  _toggleBlockType(blockType) {
+    this.onChange(
+      RichUtils.toggleBlockType(
+        this.state.editorState,
+        blockType
+      )
+    );
   }
 
   _confirmLink(e) {
@@ -112,6 +151,21 @@ class MyEditor extends React.Component {
     });
   }
 
+  _confirmImg(e) {
+    e.preventDefault();
+    const {editorState, imgSrc} = this.state;
+    const entityKey = Entity.create('IMAGE', 'IMMUTABLE', {src: imgSrc});
+    this.setState({
+      editorState: RichUtils.toggleLink(
+        editorState,
+        editorState.getSelection(),
+        entityKey
+      ),
+      showImgInput: false,
+      imgSrc: '',
+    });
+  }
+
   _promptForLink(e) {
     e.preventDefault();
     const {editorState} = this.state;
@@ -124,13 +178,50 @@ class MyEditor extends React.Component {
     }
   }
 
+  _promptForImgSrc(e) {
+    e.preventDefault();
+    const {editorState} = this.state;
+    const selection = editorState.getSelection();
+    if (!selection.isCollapsed()) {
+      this.setState({
+        showImgInput: true,
+        imgSrc: '',
+      });
+    }
+  }
+
+  _onImgInputKeyDown(e) {
+    if (e.which === 13) {
+      this._confirmImg(e);
+    }
+  }
+
+  _onLinkInputKeyDown(e) {
+    if (e.which === 13) {
+      this._confirmLink(e);
+    }
+  }
+
+  _removeLink(e) {
+    e.preventDefault();
+    const {editorState} = this.state;
+    const selection = editorState.getSelection();
+    if (!selection.isCollapsed()) {
+      this.setState({
+        editorState: RichUtils.toggleLink(editorState, selection, null),
+      });
+    }
+  }
+
   render() {
+    const {editorState} = this.state
     let urlInput;
     if (this.state.showURLInput) {
       urlInput =
         <div>
           <input
             onChange={this.onURLChange}
+            onKeyPress={this.onLinkInputKeyDown}
             ref="url"
             type="text"
             value={this.state.urlValue}
@@ -141,22 +232,166 @@ class MyEditor extends React.Component {
         </div>;
     }
 
+    let imgInput;
+    if (this.state.showImgInput) {
+      imgInput =
+        <div>
+          <input
+            onChange={this.onImgChange}
+            onKeyPress={this.onImgInputKeyDown}
+            ref="url"
+            type="text"
+            value={this.state.imgSrc}
+          />
+          <button onClick={this.confirmImg}>
+            Confirm
+          </button>
+        </div>;
+    }
+
+    let className = 'RichEditor-editor';
+    var contentState = editorState.getCurrentContent();
+    if (!contentState.hasText()) {
+      if (contentState.getBlockMap().first().getType() !== 'unstyled') {
+        className += ' RichEditor-hidePlaceholder';
+      }
+    }
+
     return (
       <div>
-        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-        <button onClick={this._onItalicClick.bind(this)}>Italic</button>
-        <button onClick={this._onUnderlineClick.bind(this)}>Underline</button>
-        <button onClick={this._promptForLink.bind(this)}>Add Link</button>
+        <button
+          onClick={this.logState}
+          type="button"
+          value="Log State"
+        />
+        <InlineStyleControls
+          editorState={editorState}
+          onToggle={this.toggleInlineStyle}
+        />
+        <BlockStyleControls
+          editorState={editorState}
+          onToggle={this.toggleBlockType}
+        />
+        <button onClick={this.promptForLink}>Add Link</button>
+        <button onClick={this.removeLink}>Remove Link</button>
+        <br/>
+        <button onClick={this.promptForImgSrc}>Add Img</button>
         { urlInput }
+        { imgInput }
         <Editor
-          editorState={this.state.editorState}
-          onChange={this.onChange}
+          blockStyleFn={getBlockStyle}
+          customStyleMap={styleMap}
+          editorState={editorState}
           handleKeyCommand={this.handleKeyCommand}
+          onChange={this.onChange}
+          onTab={this.onTab}
+          placeholder='say something dude'
+          ref="editor"
+          spellCheck={true}
         />
       </div>
     );
   }
 }
+
+const styleMap = {
+  CODE: {
+    backgroundColor: 'rgba(0, 0, 0, 0.05)',
+    fontFamily: '"Inconsolata", "Menlo", "Consolas", monospace',
+    fontSize: 16,
+    padding: 2,
+  },
+};
+
+function getBlockStyle(block) {
+  switch (block.getType()) {
+    case 'blockquote': return 'RichEditor-blockquote';
+    default: return null;
+  }
+}
+
+class StyleButton extends React.Component {
+  constructor() {
+    super();
+    this.onToggle = (e) => {
+      e.preventDefault();
+      this.props.onToggle(this.props.style);
+    };
+  }
+
+  render() {
+    let className = 'RichEditor-styleButton';
+    if (this.props.active) {
+      className += ' RichEditor-activeButton';
+    }
+
+    return (
+      <span className={className} onMouseDown={this.onToggle}>
+        {this.props.label}
+      </span>
+    );
+  }
+}
+
+const BLOCK_TYPES = [
+  {label: 'H1', style: 'header-one'},
+  {label: 'H2', style: 'header-two'},
+  {label: 'H3', style: 'header-three'},
+  {label: 'H4', style: 'header-four'},
+  {label: 'H5', style: 'header-five'},
+  {label: 'H6', style: 'header-six'},
+  {label: 'Blockquote', style: 'blockquote'},
+  {label: 'UL', style: 'unordered-list-item'},
+  {label: 'OL', style: 'ordered-list-item'},
+  {label: 'Code Block', style: 'code-block'},
+];
+
+const BlockStyleControls = (props) => {
+  const {editorState} = props;
+  const selection = editorState.getSelection();
+  const blockType = editorState
+    .getCurrentContent()
+    .getBlockForKey(selection.getStartKey())
+    .getType();
+
+  return (
+    <div className="RichEditor-controls">
+      {BLOCK_TYPES.map((type) =>
+        <StyleButton
+          key={type.label}
+          active={type.style === blockType}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      )}
+    </div>
+  );
+};
+
+var INLINE_STYLES = [
+  {label: 'Bold', style: 'BOLD'},
+  {label: 'Italic', style: 'ITALIC'},
+  {label: 'Underline', style: 'UNDERLINE'},
+  {label: 'Monospace', style: 'CODE'},
+];
+
+const InlineStyleControls = (props) => {
+  var currentStyle = props.editorState.getCurrentInlineStyle();
+  return (
+    <div className="RichEditor-controls">
+      {INLINE_STYLES.map(type =>
+        <StyleButton
+          key={type.label}
+          active={currentStyle.has(type.style)}
+          label={type.label}
+          onToggle={props.onToggle}
+          style={type.style}
+        />
+      )}
+    </div>
+  );
+};
 
 function findLinkEntities(contentBlock, callback) {
   contentBlock.findEntityRanges(
@@ -165,6 +400,19 @@ function findLinkEntities(contentBlock, callback) {
       return (
         entityKey !== null &&
         Entity.get(entityKey).getType() === 'LINK'
+      );
+    },
+    callback
+  );
+}
+
+function findImageEntities(contentBlock, callback) {
+  contentBlock.findEntityRanges(
+    (character) => {
+      const entityKey = character.getEntity();
+      return (
+        entityKey !== null &&
+        Entity.get(entityKey).getType() === 'IMAGE'
       );
     },
     callback
@@ -182,6 +430,13 @@ const Link = (props) => {
     <a href={url} style={linkStyle}>
       {props.children}
     </a>
+  );
+};
+
+const Image = (props) => {
+  const {src} = Entity.get(props.entityKey).getData();
+  return (
+    <img src={src} />
   );
 };
 
